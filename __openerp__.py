8bd680b6b5761f2f50f2fa61e8c722ddfad45edd
Bernard Delhez <bernarddelhez@gmail.com>
2016-08-19 15:03:46 +0200

    update version to 9

--- __openerp__.py
+++ __openerp__.py
@@ -1,6 +1,6 @@
 ﻿{
     'name': "Timesheet Reminder",
-    'version': '1.0',
+    'version': '9.0.1.0',
     'depends': ['hr_timesheet_sheet',],
     'author': "Bernard DELHEZ, AbAKUS it-solutions SARL",
     'website': "http://www.abakusitsolutions.eu",
